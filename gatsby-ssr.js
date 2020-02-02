e263c042ccb76aed8ef934f96fcd286cfaa93e7c
Yannis Rammos <y@rammos.co>
2020-02-02 13:46:39 +0200

    Use tracker proxy.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,7 +11,26 @@ function buildTrackingCode(pluginOptions) {
 
   var php = pluginOptions.localPHP ? pluginOptions.localPHP : '${pluginOptions.matomoUrl}/php';
 
-  var html = '\n    window.dev = ' + pluginOptions.dev + '\n    if (window.dev === true || true) {\n      window._paq = window._paq || [];\n      ' + (pluginOptions.requireConsent ? 'window._paq.push([\'requireConsent\']);' : '') + '\n      ' + (pluginOptions.disableCookies ? 'window._paq.push([\'disableCookies\']);' : '') + '\n      window._paq.push([\'setTrackerUrl\', \'' + php + '\']);\n      window._paq.push([\'setSiteId\', \'' + pluginOptions.siteId + '\']);\n      window._paq.push([\'trackPageView\']);\n      window._paq.push([\'enableLinkTracking\']);\n      window._paq.push([\'enableHeartBeatTimer\']);\n      window.start = new Date();\n\n      (function() {\n        var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];\n        g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=\'' + script + '\'; s.parentNode.insertBefore(g,s);\n      })();\n\n      if (window.dev === true) {\n        console.log(\'[Matomo] Tracking initialized\')\n\t\tconsole.log(\'[Matomo] matomoUrl: ' + pluginOptions.matomoUrl + ', siteId: ' + pluginOptions.siteId + '\')\n      }\n    }\n  ';
+  var html =
+  `
+    window._paq = window._paq || [];
+    ${requireConsent ? "window._paq.push(['requireConsent']);" : ''}
+    ${disableCookies ? "window._paq.push(['disableCookies']);" : ''}
+    window._paq.push(['enableHeartBeatTimer']);
+    window.start = new Date();
+    (function(){var u = ${pluginOptions.siteUrl};
+      _paq.push([ 'setTrackerUrl', u + ${php} ]);
+      _paq.push([ 'setSiteId', '${siteId}' ]);
+      var d = document,
+          g = d.createElement('script'),
+          s = d.getElementsByTagName('script')[0];
+      g.type = 'text/javascript';
+      g.async = true;
+      g.defer = true;
+      g.src = u + ${php};
+      s.parentNode.insertBefore(g, s);
+    })();
+  `
 
   return _react2.default.createElement('script', {
     key: 'gatsby-plugin-matomo',
@@ -20,8 +39,8 @@ function buildTrackingCode(pluginOptions) {
 }
 
 function buildTrackingCodeNoJs(pluginOptions, pathname) {
-  var html = '<img src="' + pluginOptions.matomoUrl + '/php?idsite=' + pluginOptions.siteId + '&rec=1&url=' + (pluginOptions.siteUrl + pathname) + '" style="border:0" alt="tracker" />';
-
+  // var html = '<img src="' + pluginOptions.matomoUrl + '/php?idsite=' + pluginOptions.siteId + '&rec=1&url=' + (pluginOptions.siteUrl + pathname) + '" style="border:0" alt="tracker" />';
+  var html = '<img src="" alt="" />';   // No tracking pixel.
   return _react2.default.createElement('noscript', {
     key: 'gatsby-plugin-matomo',
     dangerouslySetInnerHTML: { __html: html }
